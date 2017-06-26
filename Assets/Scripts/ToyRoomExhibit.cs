6a53da9588a46ce756f9225a174f6053bea89fb9
Dustin Peerce <dustin.a.peerce@gmail.com>
2017-06-25 23:17:33 -0400

    Added Exhibit Test Scene

---
+++ Assets/Scripts/ToyRoomExhibit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ToyRoom
+{
+
+    public class ToyRoomExhibit : MonoBehaviour
+    {
+
+        public Animator animator;
+
+        public void SetGazedAt(bool gazedAt)
+        {
+            if (gazedAt)
+            {
+                animator.SetBool(GameVals.AnimatorParameterKeys.canSeeCar, true);
+                animator.SetBool(GameVals.AnimatorParameterKeys.carIsDriving, true);
+            }
+            else
+            {
+                animator.SetBool(GameVals.AnimatorParameterKeys.canSeeCar, false);
+                animator.SetBool(GameVals.AnimatorParameterKeys.carIsDriving, false);
+            }
+        }
+
+        public void LoadToyRoom()
+        {
+            SceneManager.LoadScene("ToyRoom");
+        }
+    }
+}
