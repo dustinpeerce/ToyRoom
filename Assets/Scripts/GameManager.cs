a0e2ca026f583b421ca0df8a40b7a9554a2680c0
Dustin Peerce <dustin.a.peerce@gmail.com>
2017-09-23 17:22:56 -0500

    Moved sight and hearing logic to Person

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,37 +10,14 @@ namespace ToyRoom
     {
 
 		// Private Attributes
-		private float fovDistance;						// Field of View Distance for People
-		private float fovAngle;							// Field of View Angle for People
-        private float hearingDistance;
         private GameObject[] personPrefabs;				// Stores all Person prefabs in the scene
         private GameObject[] toyPrefabs;				// Stores all Toy prefabs in the scene
         private List<Person> personScripts;				// List of Person components on each Person prefab
         private List<Toy> toyScripts;					// List of Toy components on each Toy prefab
-        private Vector3 direction;						// The direction vector between a Person and Toy
-        private bool canSee;							// Tracks whether a Person can see a Toy
-        private bool canHear;
         private Dictionary<string, bool> animParams;	// Stores animator params for the Toy instances
 
 
-       	/// <summary>
-       	/// Awake this instance.
-       	/// </summary>
-        private void Awake()
-        {
-			// Initialize Private Attributes
-			fovDistance = 3f;
-			fovAngle = 60f;
-            hearingDistance = 2f;
-
-            CachePersonObjects();
-            CacheToyObjects();
-        }
-
-
-		/// <summary>
 		/// Caches the person objects.
-		/// </summary>
         private void CachePersonObjects()
         {
             personPrefabs = GameObject.FindGameObjectsWithTag("Person");
@@ -53,9 +30,7 @@ namespace ToyRoom
         }
 
 
-		/// <summary>
 		/// Caches the toy objects.
-		/// </summary>
         private void CacheToyObjects()
         {
             toyPrefabs = GameObject.FindGameObjectsWithTag("Toy");
@@ -68,81 +43,31 @@ namespace ToyRoom
         }
 
 
-		/// <summary>
-		/// Start this instance.
-		/// </summary>
-        private void Start()
-        {
-            StartCoroutine(TrackViewOfToys());
-        }
-
-
-		/// <summary>
 		/// Tracks the view of toys.
-		/// </summary>
-		/// <returns>The view of toys.</returns>
         private IEnumerator TrackViewOfToys()
         {
             while (true)
             {
-                    for (int p = 0; p < personPrefabs.Length; p++)
-                    {
-                        for (int t = 0; t < toyPrefabs.Length; t++)
-                        {
-                            // does the player see the toy?
-                            canSee = CanSeeToy(personPrefabs[p].transform, toyPrefabs[t].transform);
-
-                            // does the player hear the toy?
-                            canHear = CanHearToy(personPrefabs[p].transform, toyPrefabs[t].transform);
-
-                            // store the animator key-value pairs for this toy
-                            animParams = toyScripts[t].GetAnimParams();
-
-                            // Update the animator key-value pairs for the Person
-                            personScripts[p].UpdateViewParameters(animParams, canSee, canHear);
-                        }
-                        personScripts[p].ProcessTriggers();
-                    }
-
+                for (int p = 0; p < personPrefabs.Length; p++)
+                    for (int t = 0; t < toyPrefabs.Length; t++)
+                        personScripts[p].UpdateViewParameters(toyScripts[t].GetAnimParams(), toyPrefabs[t].transform);
                 yield return new WaitForSeconds(0.35f);
             }
         }
 
 
-		/// <summary>
-		/// Determines whether a given Person can see a given Toy
-		/// </summary>
-		/// <returns><c>true</c> if the Person can see the toy; otherwise, <c>false</c>.</returns>
-		/// <param name="person">Person.</param>
-		/// <param name="toy">Toy.</param>
-        private bool CanSeeToy(Transform person, Transform toy)
+        /// Awake this instance.
+        private void Awake()
         {
-            direction = toy.position - person.position;
-
-            // Detect if toy is within view distance
-            if (direction.magnitude < fovDistance)
-            {
-                // Detect if toy is within the field of view
-                if ((Vector3.Angle(direction, person.forward)) < fovAngle)
-                {
-                    return true;
-                }
-            }
-
-            return false;
+            CachePersonObjects();
+            CacheToyObjects();
         }
 
-        private bool CanHearToy(Transform person, Transform toy)
+
+        /// Start this instance.
+        private void Start()
         {
-            direction = toy.position - person.position;
-
-            // Detect if toy is within hearing distance
-            if (direction.magnitude < hearingDistance)
-            {
-                return true;
-            }
-
-            return false;
+            StartCoroutine(TrackViewOfToys());
         }
 
     } // end of class
--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+// TODO: If a person hears a toy, they should turn towards the toy but NOT start animating
+
+// TODO: If a person sees a toy, they start animating
+
+// TODO: When not responding to events, people should wander around the table according to a NavMesh
+
 namespace ToyRoom
 {
 
@@ -12,63 +18,99 @@ namespace ToyRoom
         public Animator animator;
 
         private Dictionary<string, PersonTrigger> triggers;
+        private string urgentTriggerKey;
+        private Transform currentTarget;
+        private float lookSpeed = 4.0f;
+        private float fovDistance = 3f;
+        private float fovAngle = 60f;
+        private float hearingDistance = 2f;
+
+
+        public void ClearTriggers()
+        {
+            urgentTriggerKey = "";
+            currentTarget = null;
+
+            // Reset all animator parameters to FALSE
+            foreach (var parameter in animator.parameters)
+                animator.SetBool(parameter.name, false);
+        }
+
+
+        public void ProcessTriggers()
+        {
+            // Set anim params based on most urgent trigger
+            if (urgentTriggerKey != "" && triggers[urgentTriggerKey].Rank > 0 && CanSee(currentTarget))
+            {
+                animator.SetBool(urgentTriggerKey, true);
+
+                // Set compatible triggers to their respective values
+                if (triggers[urgentTriggerKey].TriggerCombos != null)
+                    foreach (var key in triggers[urgentTriggerKey].TriggerCombos)
+                        animator.SetBool(key, triggers[key].Value);
+            }
+            else
+            {
+                animator.SetBool(GameVals.AnimParams.personDefault, true);
+
+                if (currentTarget && !CanHear(currentTarget))
+                    currentTarget = null;
+            }
+        }
+
+
+        public void UpdateViewParameters(Dictionary<string, bool> animParams, Transform target)
+        {
+            ClearTriggers();
+            foreach (var animParam in animParams)
+            {
+                triggers[animParam.Key].Value = (CanSee(target) || CanHear(target)) ? animParam.Value : false;
+
+                if (triggers[animParam.Key].Value && (urgentTriggerKey == "" || triggers[animParam.Key].Rank > triggers[urgentTriggerKey].Rank))
+                {
+                    urgentTriggerKey = animParam.Key;
+                    currentTarget = target;
+                }
+            }
+            ProcessTriggers();
+        }
+
+
+        /// Determines whether Person can see a given Toy
+        private bool CanSee(Transform toy)
+        {
+            var direction = toy.position - transform.position;
+            return (direction.magnitude < fovDistance) && (Vector3.Angle(direction, transform.forward) < fovAngle);
+        }
+
+
+        /// Determines whether Person can hear a given Toy
+        private bool CanHear(Transform toy)
+        {
+            return (toy.position - transform.position).magnitude < hearingDistance;
+        }
+
+
+        private void LookAtToy(Transform toy, float speed, float time)
+        {
+            var bodyRotation = toy.position - transform.position;
+            bodyRotation.y = 0;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(bodyRotation), time * speed);
+        }
 
 
         private void Awake()
         {
             triggers = new Dictionary<string, PersonTrigger>();
-            for(int i=0; i < GameVals.personTriggers.Length; i++)
-            {
+            for (int i = 0; i < GameVals.personTriggers.Length; i++)
                 triggers[GameVals.personTriggers[i].Name] = GameVals.personTriggers[i];
-            }
         }
 
-        public void UpdateViewParameters(Dictionary<string, bool> animParams, bool canSee, bool canHear)
+        private void FixedUpdate()
         {
-            foreach (var animParam in animParams)
-            {
-                triggers[animParam.Key].Value = (canSee || canHear) ? animParam.Value : false;
-            }
-        }
-
-        public void ProcessTriggers()
-        {
-            // Reset all animator parameters to FALSE
-            foreach (var parameter in animator.parameters)
-            {
-                animator.SetBool(parameter.name, false);
-            }
-
-            // Find the most urgent trigger
-            string maxTriggerKey = "";
-            foreach(var trigger in triggers)
-            {
-                if (trigger.Value.Value && (maxTriggerKey == "" || trigger.Value.Rank > triggers[maxTriggerKey].Rank))
-                {
-                    maxTriggerKey = trigger.Key;
-                }
-            }
-
-            // Set anim params based on most urgent trigger
-            if (maxTriggerKey != "" && triggers[maxTriggerKey].Rank > 0)
-            {
-                animator.SetBool(maxTriggerKey, true);
-
-                // Set compatible triggers to their respective values
-                if (triggers[maxTriggerKey].TriggerCombos != null)
-                {
-                    foreach (var key in triggers[maxTriggerKey].TriggerCombos)
-                    {
-                        animator.SetBool(key, triggers[key].Value);
-                    }
-                }
-            }
-            else
-            {
-                animator.SetBool(GameVals.AnimParams.personDefault, true);
-            }
+            if (currentTarget)
+                LookAtToy(currentTarget, lookSpeed, Time.deltaTime);
         }
 
     }
-
 }
